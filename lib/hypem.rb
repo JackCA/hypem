d05cc92038134cf5894101872ba6fe748893e5d5
Jack Anderson <jackcanderson@gmail.com>
2012-03-20 20:41:19 -0700

    Convenience methods for Hypem module

--- lib/hypem.rb
+++ lib/hypem.rb
@@ -1,9 +1,21 @@
-module Hypem
-
-end
-
 require "hypem/response"
 require "hypem/request"
 require "hypem/user"
 require "hypem/track"
 require "hypem/playlist"
+
+module Hypem
+
+  #convenient way of accessing module classes
+  def self.method_missing(method, *args)
+    method_name = method.capitalize
+    raise ArgumentError unless self.const_defined? method_name
+    if args.empty?
+      self.const_get(method_name)
+    else
+      self.const_get(method_name).send(:new, *args)
+    end
+  end
+
+end
+
---
+++ spec/hypem_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Hypem do
+  describe "convenience methods" do
+    it "can access playlist class" do
+      Hypem.playlist.popular.should be_a Hypem::Playlist
+    end
+
+    it "can access user class" do
+      Hypem.user('jackca').should be_a Hypem::User
+    end
+  end
+end
